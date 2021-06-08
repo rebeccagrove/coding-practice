688685eaa12b8a42100c6942f57c91c1643bd4cb
rebeccagrove <rebecca.grove92@gmail.com>
2021-06-08 08:05:10 +0100

    added javascript.js

---
+++ 03-java-script-fundamentals/javascript.js
@@ -0,0 +1,33 @@
+const name = "Andy";
+if(name == "Andy") {
+    console.log("It's a coach!")
+}
+
+const greeting = "Bonjour"; 
+if(greeting == "Bonjour") {
+    console.log("Comment ça va?")
+}
+
+const score = 10;
+if(score == 10) {
+    console.log("You've won the game!")
+}
+
+let age = 70;
+if(age >= 65) {
+    console.log("It's time to retire!")
+}
+
+const hasCovid = false;
+const isShielding = true;
+if(hasCovid || isShielding) {
+    console.log("Time to isolate")
+} else {
+    console.log("Keep social distancing")
+}
+
+age = 18;
+const isBritish = true;
+if(age >= 18 && isBritish == true) {
+    console.log("Time to vote!")
+} 
